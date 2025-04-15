4fb772746414ea4f3dd30a2ec6c8e67799240f95
Apurbo73 <apurbodebnath895@gmail.com>
2025-04-15 09:38:38 +0600

    done

---
+++ a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << pow(2.0f, 8.0f) << "\n";
+  cout << pow(3.0f, 4.0f) << "\n";
+  cout << pow(9.0, 0.5) << "\n";
+  cout << pow(8.0, -1.0) << "\n";
+  cout << pow(10.0f, -2.0f) << "\n";
+  
+// output:
+// 256
+// 81
+// 3
+// 0.125
+// 0.01
+
+  
+  
+  return 0;
+}
